e4de782b04c3abcd6188c4e8029494bca4311afb
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2024-06-18 11:59:27 -0400

    new header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import './Components/Style.css'
+import React, {useState} from 'react';
+import Header from './Components/Header';
+import SignIn from './Components/SignIn';
 
 function App() {
+
+  const [email,setEmail] = useState("maina");
   return (
     <div className="App">
-      New project
+      <Header email={email}/>
+      <SignIn/>
+
     </div>
+
+    
   );
 }
 
